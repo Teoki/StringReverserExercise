b6dd825c47e2ffd809b64541f3ce223429aea359
teomankinaci <teoman.kinaci@iubh-dualesstudium.de>
2020-07-03 17:57:57 +0200

    init commit

---
+++ spring.project/src/test/java/de/check24/teo/springproject/spring/project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.check24.teo.springproject.spring.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
